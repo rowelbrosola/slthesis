46184fbd066d8973ecf5eeb81b31eec10bcfec9f
Rowel Brosola <rowel.licagroup@gmail.com>
2020-03-11 23:21:54 +0800

    fix text

--- myapp/unit.php
+++ myapp/unit.php
@@ -55,7 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
                                     <div class="modal-dialog" role="document">
                                         <div class="modal-content">
                                             <div class="modal-header">
-                                                <h5 class="modal-title" id="rightModalLabel">Add unit member</h5>
+                                                <h5 class="modal-title" id="rightModalLabel">Add financial advisor</h5>
                                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                                             </div>
                                             <div class="modal-body">
