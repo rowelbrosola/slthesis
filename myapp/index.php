1c0245ec357acd7d474e6a4a8d1b8242a37ec1bd
Rowel Brosola <rowel.licagroup@gmail.com>
2020-04-15 01:44:43 +0800

    update calendar events

--- myapp/index.php
+++ myapp/index.php
@@ -30,6 +30,7 @@ User::isLogged();
         <link rel="stylesheet" href="css/vendor/perfect-scrollbar.css">
         <link rel="stylesheet" href="css/main.css">
         <link rel="stylesheet" href="css/vendor/fullcalendar.min.css">
+        <link rel="stylesheet" href="bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css" />
     </head>
     <body id="app-container" class="menu-default show-spinner">
         <?php include 'partials/header.php'; ?>
@@ -197,11 +198,169 @@ User::isLogged();
                             </div>
                         </div>
                     </div>
-                    <div class="col-xl-8 col-lg-12 mb-4">
+                    <div class="col-xl-6 col-lg-12 mb-4">
                         <div id="calendar" style="border: 2px solid #eee;"></div>
                     </div>
+                    <div class="col-xl-6 col-lg-12 mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Prospects (11)</h5>
+                                <div class="scroll dashboard-list-with-thumbs">
+                                    <div class="d-flex flex-row mb-3">
+                                        <div class="">
+                                            <a href="#">
+                                                <p class="list-item-heading">John Estrella</p>
+                                                <div class="pr-4 d-none d-sm-block">
+                                                    <p class="text-muted mb-1 text-small">Latashia Nagy - 100-148 Warwick Trfy, Kansas City, USA</p>
+                                                </div>
+                                                <div class="text-primary text-small font-weight-medium d-none d-sm-block">January 09, 2018</div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row mb-3">
+                                        <div class="">
+                                            <a href="#">
+                                                <p class="list-item-heading">Fruitcake</p>
+                                                <div class="pr-4 d-none d-sm-block">
+                                                    <p class="text-muted mb-1 text-small">Marty Otte - 166-156 Rue de Varennes, Gatineau, QC J8T 8G4, Canada</p>
+                                                </div>
+                                                <div class="text-primary text-small font-weight-medium d-none d-sm-block">January 09, 2018</div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row mb-3">
+                                        <div class="">
+                                            <a href="#">
+                                                <p class="list-item-heading">Chocolate Cake</p>
+                                                <div class="pr-4 d-none d-sm-block">
+                                                    <p class="text-muted mb-1 text-small">Linn Ronning - Rasen 2-14, 98547 Kühndorf, Germany</p>
+                                                </div>
+                                                <div class="text-primary text-small font-weight-medium d-none d-sm-block">January 09, 2018</div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row mb-3">
+                                        <div class="">
+                                            <a href="#">
+                                                <p class="list-item-heading">Fat Rascal</p>
+                                                <div class="pr-4 d-none d-sm-block">
+                                                    <p class="text-muted mb-1 text-small">Rasheeda Vaquera - 37 Rue des Grands Prés, 03100 Montluçon, France</p>
+                                                </div>
+                                                <div class="text-primary text-small font-weight-medium d-none d-sm-block">January 09, 2018</div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row mb-3">
+                                        <div class="">
+                                            <a href="#">
+                                                <p class="list-item-heading">Marble Cake</p>
+                                                <div class="pr-4 d-none d-sm-block">
+                                                    <p class="text-muted mb-1 text-small">Latashia Nagy - 100-148 Warwick Trfy, Kansas City, USA</p>
+                                                </div>
+                                                <div class="text-primary text-small font-weight-medium d-none d-sm-block">January 09, 2018</div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row mb-3">
+                                        <div class="">
+                                            <a href="#">
+                                                <p class="list-item-heading">Fruitcake</p>
+                                                <div class="pr-4 d-none d-sm-block">
+                                                    <p class="text-muted mb-1 text-small">Marty Otte - 166-156 Rue de Varennes, Gatineau, QC J8T 8G4, Canada</p>
+                                                </div>
+                                                <div class="text-primary text-small font-weight-medium d-none d-sm-block">January 09, 2018</div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row mb-3">
+                                        <div class="">
+                                            <a href="#">
+                                                <p class="list-item-heading">Streuselkuchen</p>
+                                                <div class="pr-4 d-none d-sm-block">
+                                                    <p class="text-muted mb-1 text-small">Mimi Carreira - 36-71 Victoria St, Birmingham, UK</p>
+                                                </div>
+                                                <div class="text-primary text-small font-weight-medium d-none d-sm-block">January 09, 2018</div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row mb-3">
+                                        <div class="">
+                                            <a href="#">
+                                                <p class="list-item-heading">Cremeschnitte</p>
+                                                <div class="pr-4 d-none d-sm-block">
+                                                    <p class="text-muted mb-1 text-small">Lenna Majeed - 6 Hertford St Mayfair, London, UK</p>
+                                                </div>
+                                                <div class="text-primary text-small font-weight-medium d-none d-sm-block">January 09, 2018</div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <!-- Modal -->
+            <div class="modal fade" id="event-modal" tabindex="-1" role="dialog" aria-labelledby="event-modal-label" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="event-modal-label">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <label for="event-title">Title</label>
+                        <input type="text" class="form-control" id="event-title" aria-describedby="titleHelp" placeholder="Enter title">
+                    </div>
+                    <div class="form-group">
+                        <label for="description">Description</label>
+                        <textarea class="form-control" id="description" rows="3"></textarea>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="timeStarts">Time Starts</label>
+                                <div class='input-group date' id='date-time-starts'>
+                                    <input type='text' name="time-starts" id="timeStarts" class="form-control" />
+                                    <span class="input-group-addon">
+                                        <span class="glyphicon glyphicon-time"></span>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="timeEnds">Time Ends</label>
+                                <div class='input-group date' id='date-time-ends'>
+                                    <input type='text' name="time-ends" id="timeEnds" class="form-control" />
+                                    <span class="input-group-addon">
+                                        <span class="glyphicon glyphicon-time"></span>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlSelect1">Who should see this?</label>
+                        <select class="form-control" id="exampleFormControlSelect1">
+                        <option>All Units</option>
+                        <option>My Unit Only</option>
+                        <option>Only Me</option>
+                        </select>
+                    </div>
 
+                </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save</button>
+                </div>
+                </div>
+            </div>
             </div>
         </main>
         <script src="js/vendor/jquery-3.3.1.min.js"></script>
@@ -215,8 +374,15 @@ User::isLogged();
         <script src="js/dore.script.js"></script>
         <script src="js/scripts.js"></script>
         <script src="js/vendor/moment.min.js"></script>
+        <script src="bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
         <script src="js/vendor/fullcalendar.min.js"></script>
         <script>
+            $(function () {
+                console.log('what the hell')
+                $('#timeStarts').datetimepicker({
+                    format: 'LT'
+                });
+            });
             var calendar = $('#calendar').fullCalendar({
                 editable:true,
                 header: {
@@ -228,23 +394,25 @@ User::isLogged();
                 selectable:true,
                 selectHelper:true,
                 select: function(start, end, allDay) {
-                    var title = prompt("Enter Event Title");
-                    if (title) {
-                        var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
-                        var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
-                        $.ajax({
-                            url:"functions/insert.php",
-                            type:"POST",
-                            data:{title:title, start:start, end:end},
-                            success:function() {
-                                calendar.fullCalendar('refetchEvents');
-                                alert("Added Successfully");
-                            },
-                            error: function(err) {
-                                console.log(err)
-                            }
-                        })
-                    }
+                    $('#event-modal').modal('show');
+                    $('#event-modal-label').html('Add an event');
+                    // var title = prompt("Enter Event Title");
+                    // if (title) {
+                    //     var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
+                    //     var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+                        // $.ajax({
+                        //     url:"functions/insert.php",
+                        //     type:"POST",
+                        //     data:{title:title, start:start, end:end},
+                        //     success:function() {
+                        //         calendar.fullCalendar('refetchEvents');
+                        //         alert("Added Successfully");
+                        //     },
+                        //     error: function(err) {
+                        //         console.log(err)
+                        //     }
+                        // })
+                    // }
                 },
                 editable:true,
                 
